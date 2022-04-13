c56f064642abc062350fe5e3499f18364ed2cca5
DavidWood2001 <50172998+DavidWood2001@users.noreply.github.com>
2022-04-13 02:37:16 +0100

    Initialised books routes

--- routes/authors.js
+++ routes/authors.js
@@ -10,31 +10,35 @@ router.get("/", async (req, res) => {
   }
   try {
     const authors = await Author.find(searchOptions);
-    res.render("authors/index", { authors: authors, searchOptions: req.query });
+    res.render("authors/index", {
+      authors: authors,
+      searchOptions: req.query,
+    });
   } catch {
     res.redirect("/");
   }
 });
 
-// New Authors Route
+// New Author Route
 router.get("/new", (req, res) => {
   res.render("authors/new", { author: new Author() });
 });
-module.exports = router;
 
-// Create Authors Route
+// Create Author Route
 router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
   });
   try {
     const newAuthor = await author.save();
-    // res.redirect('authors/${newAuthor.id');
-    res.redirect("authors");
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`authors`);
   } catch {
     res.render("authors/new", {
       author: author,
-      errorMessage: "Error creating author",
+      errorMessage: "Error creating Author",
     });
   }
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
@@ -8,6 +9,7 @@ const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -17,15 +19,13 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-});
-
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.error("Connected to Mongoose"));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.listen(process.env.PORT || 3000);
